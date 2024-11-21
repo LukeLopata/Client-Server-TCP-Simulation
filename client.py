d7668a36ab4fae95da76800f404e715e583f7b3b
Luke Lopata <lukrlopat@gmail.com>
2024-11-20 20:29:50 -0600

    disc and reconnect is emplamented on the client side, not the server side yet

--- client.py
+++ client.py
@@ -1,11 +1,9 @@
 import socket
 import queue
 import threading
+import time
 
 
-
-
-CLIENTPORTNUMBER = 5678
 SERVERPORTNUMBER = 1234
 
 SUB_ACK = threading.Event()
@@ -13,12 +11,12 @@ PUB_EROR = threading.Event()
 CONN_ACK = threading.Event()
 DISC_ACK = threading.Event()
 SUB_FAILED = threading.Event()
-
-
+END_SENDER = threading.Event()
 
 def recevie_messages(client_socket):
     try:
         while True:
+            time.sleep(0.1)
             message = client_socket.recv(1024).decode()
             print(f"[{message}]")
             if message == "CONN_ACK":
@@ -29,28 +27,36 @@ def recevie_messages(client_socket):
                 SUB_FAILED.set()
             elif message == "DISC_ACK":
                 DISC_ACK.set()
+                END_SENDER.set()
+                messages_out.put("") # put an empty/dummy message in the out Que to make ure the thread closes
+                break   # break to the finally of the try block to handle ending this thread
             elif message == "ERROR: Not Subscribed" :
                 PUB_EROR.set()
                 print("tried to publish info to a topic we are not subscribed to")
-            else:
-                messages_in.put(message)
+            # else:   # put messages that are not ACK in a queue to be stored/processed if we want more than just printing them
+            #     messages_in.put(message)
     except Exception as e:
         print(f"ERROR {e} in recevie_messages")
     finally:
-        client.close()
-        exit()
+        print("Ending receive_message thread")
+        
             
                 
-def send_messages():
+def send_messages(current_client):
+    print("in send message")
+    END_SENDER.clear()
     while True:
         try:
-            message = messages_out.get() # blocks until there is a message in the Que
-            client.send(message.encode())
-        except:
-            print("Error in sending messages")
+            message = messages_out.get() # waits for message in Que. Only blocks for 1 second
+            if END_SENDER.is_set():
+                break  
+            current_client.send(message.encode())
+            
+        except Exception as e:
+            print(f"ERROR {e} in send_messages")
             break
 
-        
+
 def subscribe(topic):
     SUB_ACK.clear()
     SUB_FAILED.clear()
@@ -64,86 +70,98 @@ def subscribe(topic):
     else: 
         print(f"SUB to {topic} failed")
 
-        
-    
+
+
 def publish(subject, message):
     messages_out.put(f"{client_name}, PUB, {subject}, {message}\n")
-    
+
 def disconnect():
     messages_out.put("DISC\n")
     while not DISC_ACK.wait(timeout = 3):
         print("DISC timeout, resending")
         messages_out.put("DISC\n")
     print("Succsefully disconnected")
-    
+
 def connect(client_name):
-    #
-    CONN_ACK.clear()
-    messages_out.put(f"{client_name}, CONN\n")
-    while not CONN_ACK.wait(timeout = 3):
-        print("Connection not ACK'ed, trying again")
+    try:
+        print("Starting Client")    
+        
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow reusing the port
+        client.connect(("localhost", SERVERPORTNUMBER))
+
+        # start sending and reciveing threads
+        recevie_thread = threading.Thread(target = recevie_messages, args=([client]))
+        recevie_thread.daemon = True
+        recevie_thread.start()
+        
+        send_thread = threading.Thread(target = send_messages, args=([client]))
+        send_thread.daemon = True
+        send_thread.start()
+        
+        CONN_ACK.clear()
         messages_out.put(f"{client_name}, CONN\n")
-        
-    print("Client connected")
-
-        
-
-    
-
-
+        while not CONN_ACK.wait(timeout = 3):
+            print("Connection not ACK'ed, trying again")
+            messages_out.put(f"{client_name}, CONN\n")
+            
+        print("Client connected")
+        return True
+    except Exception as e:
+        print(f"Failed to connect with Error {e}")
+        return False
 
 if __name__ == "__main__":
     messages_out = queue.Queue()
     messages_in = queue.Queue()
-    # acknowledgments = queue.Queue()
     
     # get user info
-    client_name = input("What is the name of this client?\n")
-    # client_name = "Client Number 1"
+    # lient_name = input("What is the name of this client?\n")
+    client_name = "Client Number 1"
     
-    CLIENTPORTNUMBER = int(input("What is the client port number\n"))
-    # CLIENTPORTNUMBER = 5678
+    # CLIENTPORTNUMBER = int(input("What is the client port number\n"))
+    CLIENTPORTNUMBER = 5678
 
-    
-    # setup socket
-    print("Starting Client")
+
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow reusing the port
-    
-    client.connect(("localhost", SERVERPORTNUMBER))
 
     
-    # start sending and reciveing threads
-    recevie_thread = threading.Thread(target = recevie_messages, args=([client]))
-    recevie_thread.daemon = True
-    recevie_thread.start()
-    
-    send_thread = threading.Thread(target = send_messages, args=([]))
-    send_thread.daemon = True
-    send_thread.start()
-    
-    
+    # setup socket and connect
+    # print("Starting Client")
+    # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow reusing the port
+    # client.connect(("localhost", SERVERPORTNUMBER))
+
+    # # start sending and reciveing threads
+    # recevie_thread = threading.Thread(target = recevie_messages, args=([client]))
+    # recevie_thread.daemon = True
+    # recevie_thread.start()
+
+    # send_thread = threading.Thread(target = send_messages, args=([]))
+    # send_thread.daemon = True
+    # send_thread.start()
+
     connect(client_name)
-    running = True
-    while running:
-        
-        action = input("What do you want to do. Options: sub, pub, disc\n")
-        
-        if action.lower() == "sub":
-            topic = input("What topic do you want to subscribe to?\n")
-            subscribe(topic.upper())        
-        elif action.lower() == "pub":
-            subject = input("What topic do you want to publish to?\n")
-            message = input("What message would you like to send?\n")
-            publish(subject, message)
-        elif action.lower() == "disc":
-            disconnect()      
-        # elif action.lower() == "reconn":
-        #     connect( client_name)
 
     
-    
-    
-
-    # Close the connection
-    client.close(SERVERPORTNUMBER, client_name)
+    connected = True
+    while True:
+        if (connected):
+            action = input("What do you want to do. Options: sub, pub, disc\n")
+            if action.lower() == "sub":
+                topic = input("What topic do you want to subscribe to?\n")
+                subscribe(topic.upper())        
+            elif action.lower() == "pub":
+                subject = input("What topic do you want to publish to?\n")
+                message = input("What message would you like to send?\n")
+                publish(subject, message)
+            elif action.lower() == "disc":
+                connected = False
+                disconnect()
+                client.close()      
+        else: # not connected
+            action = input("Would you like to reconnect? [Y/N]\n")
+            if action.lower() == "yes" or action.lower() == "y":
+                connected = connect(client_name)
+                
+            
--- server.py
+++ server.py
@@ -10,8 +10,8 @@ class Client:
         self.address = address
         self.subscription = {"NEWS" : False, "WEATHER" : False}
         self.offline = False    #for phase 2
-        
-        
+
+
 PORTNUMBER = 1234
 clients = []
 clientlock = threading.Lock()
@@ -26,19 +26,18 @@ all_weather = []
 
 def handleClient(client_socket, client_address):
     client = Client(client_socket, client_address)
-    
+
     with clientlock:
         clients.append(client)
     # handle connection message seperatly
-    
+
     message = client.socket.recv(1024).decode()
     print(message)
     message = message.split(",")
-    if message[1] and message[1].strip() != "CONN":
+    if len(message) <= 1 or message[1].strip() != "CONN":
         print(f"ERROR: First message recieved is not a CONN: {message}")
     print(message)
     client.socket.send("CONN_ACK".encode())
-    
     client.name = message[0]
 
     try:
@@ -46,11 +45,11 @@ def handleClient(client_socket, client_address):
             message = client.socket.recv(1024).decode()
             if not message:
                 break
-            
+
             print(f"[MESSAGE from {client.address}]: {message}")
             message = message.split(",")
             message = [elem.strip() for elem in message] # strip all extra white space from the message
-            
+
             if(message[0] == "DISC"):
                 client.socket.send("DISC_ACK".encode())
                 break
@@ -58,7 +57,7 @@ def handleClient(client_socket, client_address):
             elif (message[0] != client.name):
                 print("ERROR, name in message doesnt match name on file")
                 print(f"On file: {client.name} Recieved: {message[0]}")
-                
+
             elif (message[1] == "SUB"):
                 if (message[2].upper()) in client.subscription:
                     client.subscription[message[2].upper()] = True
@@ -76,8 +75,8 @@ def handleClient(client_socket, client_address):
                     client.socket.send("ERROR: Subject Not Found".encode())
             else:
                 print(f"ERROR: message tag {message[1]} unknown")
-                
-            
+
+
     except Exception as e:
         print(f"ERROR {e}")
     finally:
@@ -85,7 +84,7 @@ def handleClient(client_socket, client_address):
         print(f"[INFO] Connection clsoed {client_address}")
         exit()
 
-     
+
 def weatherNotifier():
     while True:
         notification = notifications["WEATHER"].get() # this will block until there is something to grab
@@ -98,8 +97,8 @@ def weatherNotifier():
                         print("sending message ")
                         client.socket.send(f"NOTICICATION, WEATHER, {notification}".encode())
         all_weather.append(notification)
-            
-                     
+
+
 def newsNotifier():
     while True:
         notification = notifications["NEWS"].get() # this will block until there is something to grab
@@ -112,8 +111,8 @@ def newsNotifier():
                         print("sending message ")
                         client.socket.send(f"NOTICICATION, NEWS, {notification}".encode())
         all_news.append(notification)
-    
-    
+
+
 
 
 print("Starting Server")
@@ -122,7 +121,7 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow reusing th
 server.bind(('localhost', PORTNUMBER))
 server.listen()
 
-#make notification thread   
+# make notification threads   
 news_notification_thread = threading.Thread(target=newsNotifier, args=())
 news_notification_thread.daemon = True
 news_notification_thread.start()
@@ -137,7 +136,7 @@ try:
         print("Waiting for connection")
         client_socket, client_address = server.accept()
         print("Connection accepted")
-        
+
         thread = threading.Thread(target=handleClient, args=(client_socket, client_address))
         thread.daemon = True
         thread.start()
